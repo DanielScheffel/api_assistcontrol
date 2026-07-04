b0eb3a2b188119f6018e1f28b0f24dd09c61071e
DanielScheffel <danioliveira.8292@gmail.com>
2026-07-04 12:20:30 -0300

    Corrigindo caminho de uploads no render

--- src/models/assistenciaImagemModel.js
+++ src/models/assistenciaImagemModel.js
@@ -3,16 +3,18 @@ import pool from "../config/database.js";
 
 // Cria uma nova imagem de assistência
 export async function criarImagemAssistencia( assistenciaID, file ) {
+    const urlArquivo = `uploads/${file.filename}`;
+
     await pool.query(
         `INSERT INTO assistencia_imagem (assistencia_id, nome_arquivo, url_arquivo)
         VALUES ($1, $2, $3)`,
-        [assistenciaID, file.filename, file.path]
+        [assistenciaID, file.filename, urlArquivo]
     )
 
     return {
         message: "Imagem de assistência cadastrada com sucesso",
         assistenciaID,
         nome_arquivo: file.filename,
-        url_arquivo: file.path
+        url_arquivo: urlArquivo
     }
 }
